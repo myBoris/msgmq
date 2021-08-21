82ebfaf1c12f5185286ff27f1f0518d9249ab004
myBoris <3077534618@qq.com>
2021-08-21 16:04:22 +0900

    init  commit

---
+++ src/main/java/com/msgmq/app/config/ActiveMqConfig.java
@@ -0,0 +1,15 @@
+package com.msgmq.app.config;
+
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.jms.JmsPoolConnectionFactoryFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.jms.ConnectionFactory;
+
+public class ActiveMqConfig {
+
+    //@Bean
+    //public JmsPoolConnectionFactoryFactory jmsListenerContainerQueue(ConnectionFactory activeMQConnectionFactory){
+
+   // }
+}
